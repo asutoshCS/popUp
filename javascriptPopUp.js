e86a67aee16d0c6f5470459a8a418d070e7bf4b2
Asu Dhakal <asutosh_dhakal@yahoo.com>
2016-04-17 04:04:29 -0500

    test

--- javascriptPopUp.js
+++ javascriptPopUp.js
@@ -7,13 +7,11 @@ document.getElementById('textBoxId').style.fontSize="14pt";
 
 function myFunction() {
     var aut= document.getElementById("author").value;
-    var datP= document.getElementById("dateP").value;
+    var url= document.getElementById("url").value;
     var dat= document.getElementById("date").value;
     var tit= document.getElementById("title").value;
-    var wnm= document.getElementById("wName").value;
-    var wp= document.getElementsById("wPub").value;
     var space = ' ';
-    var total = aut.concat(space,tit,space,wnm,space,wp,space,datP,space,dat);
+    var total = aut.concat(space,tit,space,dat,space,url);
     document.getElementById("citation").innerHTML = total;
 }
 

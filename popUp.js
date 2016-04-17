bcc0fb43feb66fa8b04bf3f5efab435aaba4596b
Asu Dhakal <asutosh_dhakal@yahoo.com>
2016-04-16 20:08:52 -0500

    test

--- popUp.js
+++ popUp.js
@@ -1,5 +1,3 @@
-
-
 var url = document.getElementById("url").value;
 var title = document.getElementById("title").value;
 var author = document.getElementById("authName").value;
@@ -7,8 +5,7 @@ var date = document.getElementById("date").value;
 
 function createMLA() {
 
-	var message = 'url' + 'title' + 'authName' + 'date';
+	var message = 'url'; 
 
 	return message; 
-
 }

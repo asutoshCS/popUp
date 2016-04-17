759bbe765c731082d442fdafbac2b61653c01016
Asu Dhakal <asutosh_dhakal@yahoo.com>
2016-04-17 03:56:39 -0500

    test

--- javascriptPopUp.js
+++ javascriptPopUp.js
@@ -17,20 +17,5 @@ function myFunction() {
     document.getElementById("citation").innerHTML = total;
 }
 
-(function () {
-var textFile = null,
-  makeTextFile = function (text) {
-    var data = new Blob([text], {type: 'text/plain'});
-
-    // If we are replacing a previously generated file we need to
-    // manually revoke the object URL to avoid memory leaks.
-    if (textFile !== null) {
-      window.URL.revokeObjectURL(textFile);
-    }
-
-    textFile = window.URL.createObjectURL(data);
-
-    return textFile;
-  };
 
 

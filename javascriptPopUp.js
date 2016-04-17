dcf1d118c7b2476eb386a8826b1adf41c49a01b6
Asu Dhakal <asutosh_dhakal@yahoo.com>
2016-04-17 05:34:27 -0500

    test

--- javascriptPopUp.js
+++ javascriptPopUp.js
@@ -16,53 +16,23 @@ function myFunction() {
     var total = aut.concat(space,tit,space,webT,space,webP,space,datP,space,datA);
     document.getElementById("citation").innerHTML = total;
 }
-
-function saveTextAsFile()
-{      
-// grab the content of the form field and place it into a variable
-    var textToWrite = document.getElementById("inputTextToSave").value;
-//  create a new Blob (html5 magic) that conatins the data from your form feild
-    var textFileAsBlob = new Blob([textToWrite], {type:'text/plain'});
-// Specify the name of the file to be saved
-    var fileNameToSaveAs = "myNewFile.txt";
-    
-// Optionally allow the user to choose a file name by providing 
-// an imput field in the HTML and using the collected data here
-// var fileNameToSaveAs = txtFileName.text;
-
-// create a link for our script to 'click'
-    var downloadLink = document.createElement("a");
-//  supply the name of the file (from the var above).
-// you could create the name here but using a var
-// allows more flexability later.
-    downloadLink.download = fileNameToSaveAs;
-// provide text for the link. This will be hidden so you
-// can actually use anything you want.
-    downloadLink.innerHTML = "My Hidden Link";
-    
-// allow our code to work in webkit & Gecko based browsers
-// without the need for a if / else block.
-    window.URL = window.URL || window.webkitURL;
-          
-// Create the link Object.
-    downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
-// when link is clicked call a function to remove it from
-// the DOM in case user wants to save a second file.
-    downloadLink.onclick = destroyClickedElement;
-// make sure the link is hidden.
-    downloadLink.style.display = "none";
-// add the link to the DOM
-    document.body.appendChild(downloadLink);
-    
-// click the new link
-    downloadLink.click();
-}
-
-function destroyClickedElement(event)
-{
-// remove the link from the DOM
-    document.body.removeChild(event.target);
-}
-
-// EOF
-
+$("button").click(function() {
+  // create `a` element
+  $("<a />", {
+      // if supported , set name of file
+      download: $.now() + ".txt",
+      // set `href` to `objectURL` of `Blob` of `textarea` value
+      href: URL.createObjectURL(
+        new Blob([$("textarea").val()], {
+          type: "text/plain"
+        }))
+    })
+    // append `a` element to `body`
+    // call `click` on `DOM` element `a`
+    .appendTo("body")[0].click();
+    // remove appended `a` element after "Save File" dialog,
+    // `window` regains `focus` 
+    $(window).one("focus", function() {
+      $("a").last().remove()
+    })
+})

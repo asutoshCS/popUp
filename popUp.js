3fc35cf81305c4aee04df3f197b13179abe57a66
Asu Dhakal <asutosh_dhakal@yahoo.com>
2016-04-16 19:07:07 -0500

    ...

---
+++ popUp.js
@@ -0,0 +1,13 @@
+
+var url = popup.getElementByid('url');
+var title = popup.getElementByid('title');
+var author = popup.getElementByid('authName');
+var date = popup.getElementByid('date');
+
+function MLA() {
+
+	var message = 'url'; 
+
+	return message; 
+
+}

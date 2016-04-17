ce2fbff42980ccd56f464bcb4d05cf78724dc568
Asu Dhakal <asutosh_dhakal@yahoo.com>
2016-04-17 01:36:20 -0500

    test

--- javascriptPopUp.js
+++ javascriptPopUp.js
@@ -1,7 +1,13 @@
+document.addEventListener('DOMContentLoaded', function () {
+      document.querySelector('button').addEventListener('click', main);      
+});
 
-
-function createMLA() {
-
-	document.write(document.getElementById('url').value + "</br>"); 
-
+function myFunction() {
+    var aut= document.getElementById("author").value;
+    var url= document.getElementById("url").value;
+    var dat= document.getElementById("date").value;
+    var tit= document.getElementById("title").value;
+    var space = ' ';
+    var total = tit.concat(space,aut,space,dat,space,url);
+    document.getElementById("citation").innerHTML = total;
 }
